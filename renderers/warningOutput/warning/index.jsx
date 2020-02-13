f1b64f9782c772ef1c9184f12a7f545b799ef972
Bomdi <dzedock@yahoo.com>
2020-02-13 16:42:20 +0200

    update Warning styling

--- renderers/warningOutput/warning/index.jsx
+++ renderers/warningOutput/warning/index.jsx
@@ -14,16 +14,16 @@ import PropTypes from 'prop-types';
 //#endregion
 
 const Warning = ({ message, customStyle }) => (
-  <div style={ warningStyle.warningStyle }>
-    <span style={ warningStyle.contentStyle }>
-      <svg style={ warningStyle.iconStyle }  version='1.1' id='Capa_1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' x='0px' y='0px'
+  <div style={ warningStyle.warning }>
+    <span style={ warningStyle.content }>
+      <svg style={ warningStyle.icon }  version='1.1' id='Capa_1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' x='0px' y='0px'
           viewBox='0 0 50 50' style='enable-background:new 0 0 50 50;' xmlSpace='preserve'>
-        <circle style='fill:#EFCE4A;' cx='25' cy='25' r='25'/>
-        <line style='fill:none;stroke:#FFFFFF;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;' x1='25' y1='10' x2='25' y2='32'/>
-        <line style='fill:none;stroke:#FFFFFF;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;' x1='25' y1='37' x2='25' y2='39'/>
+        <circle style={ warningStyle.circle } cx='25' cy='25' r='25'/>
+        <line style={ warningStyle.line } x1='25' y1='10' x2='25' y2='32'/>
+        <line style={ warningStyle.line } x1='25' y1='37' x2='25' y2='39'/>
         <g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
       </svg>
-      <p style={{ ...warningStyle.messageStyle, ...customStyle }}>{ message }</p>
+      <p style={{ ...warningStyle.message, ...customStyle }}>{ message }</p>
     </span>
   </div>
 );
--- renderers/warningOutput/warning/warningStyle.js
+++ renderers/warningOutput/warning/warningStyle.js
@@ -1,7 +1,7 @@
 import { fontFamily } from '../../../style/template';
 
 const warningStyle = {
-  warningStyle: {
+  warning: {
     width: '100%',
     margin: '5px 0',
     display: 'flex',
@@ -9,7 +9,7 @@ const warningStyle = {
     alignItems: 'center',
     fontFamily
   },
-  contentStyle: {
+  content: {
     minWidth: '240px',
     margin: '5px 0',
     padding: '0 8px',
@@ -19,15 +19,25 @@ const warningStyle = {
     flexWrap: 'wrap',
     fontFamily
   },
-  iconStyle: {
+  icon: {
     marginRight: '10px',
     width: '20px',
     height: '20px',
     fontFamily
   },
-  messageStyle: {
+  message: {
     fontFamily,
     fontWeight: 600
+  },
+  circle: {
+    fill: '#EFCE4A'
+  },
+  line: {
+    fill: 'none',
+    stroke: '#FFFFFF',
+    strokeWidth: 2,
+    strokeLinecap: 'round',
+    strokeMiterlimit: 10
   }
 };
 
